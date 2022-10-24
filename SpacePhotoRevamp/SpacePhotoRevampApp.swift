5f5433ef8bc1e718a0a0ad7f8db1541dd286e93f
Brian Aguirre <aguirre.28@osu.edu>
2022-10-23 21:23:45 -0400

    Initial Commit

---
+++ SpacePhotoRevamp/SpacePhotoRevampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacePhotoRevampApp.swift
+//  SpacePhotoRevamp
+//
+//  Created by Aguirre, Brian P. on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct SpacePhotoRevampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
